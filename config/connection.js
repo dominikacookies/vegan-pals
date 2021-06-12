00398120dd910506477eeac8dd251f3f24ef5b05
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-12 13:40:21 +0100

    sequelize connection

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,17 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
-
-module.exports = sequelize;
+const sequelize = require("sequelize");
+const dbOptions = {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+  port: process.env.DB_PORT,
+  logging: false,
+};
+const dbName = process.env.DB_NAME;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = new sequelize(process.env.JAWSDB_URL);
+} else {
+  connection = new sequelize(dbName, dbUser, dbPassword, dbOptions);
+}
+module.exports = connection;
