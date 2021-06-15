2e457b14c213cff6e0ecfd39a73cd88f93a15475
Misha <73886737+misha244@users.noreply.github.com>
2021-06-15 21:53:41 +0100

    add cooktogether associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,10 @@
 const User = require("./User");
 const Recipe = require("./Recipe");
+const CookTogether = require("./CookTogether");
 
 User.hasMany(Recipe, { foreignKey: "user_id" });
 Recipe.belongsTo(User, { foreignKey: "user_id" });
+CookTogether.belongsTo(User, { foreignKey: "user_id" });
+User.hasMany(CookTogether, { foreignKey: "user_id" });
 
-module.exports = { User, Recipe };
+module.exports = { User, Recipe, CookTogether };
