06a1a2948c63d1f30a580558fbb87afd9950e9c2
Misha <73886737+misha244@users.noreply.github.com>
2021-06-15 21:53:22 +0100

    add cooktogether to seed

--- src/db/seeds/seed.js
+++ src/db/seeds/seed.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 
 const sequelize = require("../../config/connection");
-const { User, Recipe } = require("../../models");
+const { User, Recipe, CookTogether } = require("../../models");
 const users = require("./data/users.json");
 const recipes = require("./data/myRecipes.json");
+const cookTogether = require("./data/cookTogether.json");
 
 const seed = async () => {
   await sequelize.sync({ force: true });
@@ -13,6 +14,9 @@ const seed = async () => {
 
   await Recipe.bulkCreate(recipes);
   console.log("Successfully seeded recipes");
+
+  await CookTogether.bulkCreate(cookTogether);
+  console.log("Successfully seeded cook togethers");
 
   process.exit(0);
 };
