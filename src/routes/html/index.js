5064dabafe5246398adfbd63abdcceb6d359b257
Otilia Visan <otilia.visan22@gmail.com>
2021-06-17 20:08:51 +0100

    created the private routes/handlebars

--- src/routes/html/index.js
+++ src/routes/html/index.js
@@ -2,11 +2,11 @@ const { Router } = require("express");
 
 const publicRoutes = require("./public");
 const privateRoutes = require("./private");
-const auth = require("../../middlewares/auth");
+//const auth = require("../../middlewares/auth");
 
 const router = Router();
 
 router.use(publicRoutes);
-router.use(auth, privateRoutes);
+router.use(privateRoutes);
 
 module.exports = router;
