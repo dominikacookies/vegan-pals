12a9b492f86ff685c431c35313edbcc732a23027
Dominika <domi_nika@hotmail.co.uk>
2021-06-17 19:09:20 +0100

    throws 404 error if requested params not in post body

--- src/controllers/api/index.js
+++ src/controllers/api/index.js
@@ -1,12 +1,65 @@
-// get all cooktogether information specifc to a user and group response by sent, accepted and received
+const { CookTogether, User, Recipe } = require("../../models")
+
+// get all cooktogether information specific to a user and group response by sent, accepted and received
 const getAllCookTogethers = async (req, res) => {
-  console.log("getting all cook together")
+
+  // TO DO: get user id from session
+
+  try {
+  // get all received
+  const requestedCookTogether = await CookTogether.findAll({
+    where: {
+      user_id: 1,
+      status: "received",
+    },
+    raw: true,
+    nested: true,
+  })
+
+  // get all sent
+  const sentCookTogether = await CookTogether.findAll({
+    where: {
+      user_id: 1,
+      status: "sent",
+    },
+    raw: true,
+    nested: true,
+  })
+  
+  const upcomingCookTogether = await CookTogether.findAll({
+    where: {
+      user_id: 1,
+      status: "accepted",
+    },
+    raw: true,
+    nested: true,
+  })
+  
+  const cookTogetherData = {
+    requestedCookTogether,
+    sentCookTogether,
+    upcomingCookTogether
+  }
+
+  res.status(200).json(cookTogetherData)
+    
+  } catch (error) {
+    res.status(500).json({
+      error: "Could not get cooktogethers"
+    })
+  }
 }
 
 // create new cooktogether
 // if somebody creates a new request
 const createCookTogether = async (req, res) => {
-  console.log("creating cook together")
+  const { dateTime, mealType, message, contactDetailsForSendingUser, userIdReceivingInvite, recipeId, recipeTitle } = req.body
+  
+  if (!dateTime || !mealType || !message || !contactDetailsForSendingUser || !userIdReceivingInvite || !recipeId || !recipeTitle) {
+    res.status(404).json({
+      error: "Missing parameters",
+    })
+  }
 }
 
 // update a cooktogether
