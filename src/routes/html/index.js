dcf12118839aecebcb3ac23e6c00af7473eae9bf
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-17 19:52:07 +0100

    auth middleware added

--- src/routes/html/index.js
+++ src/routes/html/index.js
@@ -2,11 +2,11 @@ const { Router } = require("express");
 
 const publicRoutes = require("./public");
 const privateRoutes = require("./private");
-// const auth = require("");
+const auth = require("../../middlewares/auth");
 
 const router = Router();
 
 router.use(publicRoutes);
-// router.use(auth, privateRoutes);
+router.use(auth, privateRoutes);
 
 module.exports = router;
