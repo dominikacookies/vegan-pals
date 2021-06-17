70a2a8b23f6d291f6bd0b03e4db959ef75b9ccea
Dominika <domi_nika@hotmail.co.uk>
2021-06-17 15:17:12 +0100

    added auth and api routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,18 @@
 const { Router } = require("express");
 
-const apiRoutes = require("../controllers/api");
-const userRoutes = require("../controllers/userRoutes")
+const apiRoutes = require("./api");
+const authRoutes = require("./auth")
 
 const router = Router();
 
 router.use("/api", apiRoutes);
 
-router.use("/user", userRoutes)
+routers.use("/auth", authRoutes)
 
+//not sure what the below is. commenting out for now.
+// router.use("/user", userRoutes)
+
+// what does this do?
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
 });
