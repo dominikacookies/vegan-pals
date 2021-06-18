6afcb10cf9ef55a09d3675b5f97bd1b4786d3f01
Otilia Visan <otilia.visan22@gmail.com>
2021-06-18 19:02:23 +0100

    added middleware and sudo-code for private route

--- src/controllers/html/private.js
+++ src/controllers/html/private.js
@@ -1,10 +1,31 @@
-const renderCookTogether = (req, res) => {
+const renderCookTogether = async (req, res) => {
+  // try {
+  //   //const { cookTogether, myRecipes, myPals } = req.session;
+  //   const getUsers = await User.findAll({
+  //     where: {
+  //       user_id: userId,
+  //     },
+  //     include: [
+  //       {
+  //         model: User,
+  //       },
+  //     ],
+  //   });
+
+  //   const formattedgetUsers = allUsers.map((getUsers) =>
+  //     getUsers.get({ plain: true })
+  //   );
+  // } catch (error) {}
   res.render("cooktogether");
 };
-const renderMyRecipesCookTogether = (req, res) => {
+const renderMyRecipesCookTogether = async (req, res) => {
+  try {
+  } catch (error) {}
   res.render("myrecipes-cooktogether");
 };
-const renderMyRecipes = (req, res) => {
+const renderMyRecipes = async (req, res) => {
+  try {
+  } catch (error) {}
   res.render("myrecipes");
 };
 
