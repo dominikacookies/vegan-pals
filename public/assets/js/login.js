e8eac64cbe9a9c55be07bf433b57bd883edc8e98
Dominika <domi_nika@hotmail.co.uk>
2021-06-18 21:07:09 +0100

    handles login verifaction response

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -10,10 +10,39 @@ const handleSubmit = async (event) => {
     $("#login-error-messages").append(`
     <p class="login-error-message "> Please enter an email and password to login </p>
     `)
+    return
   }
 
-  console.log(email)
-  console.log(password)
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    redirect: "follow",
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  };
+
+  const response = await fetch("http://localhost:3001/auth/login", options);
+  console.log(response)
+
+  if (response.status == 200) {
+    window.location.replace("http://localhost:3001/"); 
+  } else if (response.status == 404) {
+    $("#login-error-messages").empty()
+    $("#login-error-messages").append(`
+    <p class="login-error-message "> Email or password incorrect, please try again. </p>
+    `)
+  } else {
+    $("#login-error-messages").empty()
+    $("#login-error-messages").append(`
+    <p class="login-error-message "> Oops! We're having some trouble at the moment. Please try again later. </p>
+    `)
+  }
+
+  console.log(response)
 } 
 
 const renderSignUpPage = (event) => {
