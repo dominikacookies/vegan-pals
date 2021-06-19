116bf6b75c0ec7466f93ea80555fb52234ff8e47
Otilia Visan <otilia.visan22@gmail.com>
2021-06-19 15:15:27 +0100

    added the sequelize to grab users with the same intolerances

--- src/controllers/html/private.js
+++ src/controllers/html/private.js
@@ -1,22 +1,36 @@
 const renderCookTogether = async (req, res) => {
-  // try {
-  //   //const { cookTogether, myRecipes, myPals } = req.session;
-  //   const getUsers = await User.findAll({
-  //     where: {
-  //       user_id: userId,
-  //     },
-  //     include: [
-  //       {
-  //         model: User,
-  //       },
-  //     ],
-  //   });
+  try {
+    //const { cookTogether, myRecipes, myPals } = req.session;
+    const {
+      intolerances: { gluten, peanuts, sesame, grain, soy, sulphite, wheat },
+    } = req.session.user;
 
-  //   const formattedgetUsers = allUsers.map((getUsers) =>
-  //     getUsers.get({ plain: true })
-  //   );
-  // } catch (error) {}
-  res.render("cooktogether");
+    const matchingUsers = await User.findAll({
+      where: {
+        [Op.and]: [
+          { gluten_intolerance: gluten },
+          { grain_intolerance: grain },
+          { soy_intolerance: soy },
+          { peanuts_intolerance: peanuts },
+          { sesame_intolerance: sesame },
+          { sulphite_intolerance: sulphite },
+          { tree_nut_intolerance: req.session.user.intolerances["tree nut"] },
+          { wheat_intolerance: wheat },
+        ],
+      },
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    console.log(matchingUsers);
+
+    //   const formattedgetUsers = allUsers.map((getUsers) =>
+    //     getUsers.get({ plain: true })
+    //   );
+    res.render("cooktogether");
+  } catch (error) {}
 };
 const renderMyRecipesCookTogether = async (req, res) => {
   try {
