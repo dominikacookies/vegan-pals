092669a9079d5281ecf50926d784fd359ce48c9c
Dominika <domi_nika@hotmail.co.uk>
2021-06-19 20:39:34 +0100

    gets all users recipes and passes onto handlebar

--- src/controllers/html/private.js
+++ src/controllers/html/private.js
@@ -1,11 +1,23 @@
+const { Recipe } = require("../../models");
+
 const renderCookTogether = (req, res) => {
   res.render("cooktogether");
 };
 const renderMyRecipesCookTogether = (req, res) => {
   res.render("myrecipes-cooktogether");
 };
-const renderMyRecipes = (req, res) => {
-  res.render("myrecipes");
+const renderMyRecipes = async (req, res) => {
+  const recipes = await Recipe.findAll({
+    attributes: ['recipe_id', 'dish_name'],
+    where: {
+      user_id: req.session.user.id
+    },
+    raw: true,
+    nested: true
+  })
+
+  console.log(recipes)
+  res.render("myrecipes", {recipes})
 };
 
 module.exports = {
