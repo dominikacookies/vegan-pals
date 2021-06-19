75358c71165a857c14ffa3e689feae1f97f61b07
Dominika <domi_nika@hotmail.co.uk>
2021-06-19 21:05:51 +0100

    logic to get users with same intolerances

--- src/controllers/html/private.js
+++ src/controllers/html/private.js
@@ -1,11 +1,43 @@
-const { Recipe } = require("../../models");
+const { Recipe, User } = require("../../models");
+const {Op} = require("sequelize")
 
 const renderCookTogether = (req, res) => {
   res.render("cooktogether");
 };
+
 const renderMyRecipesCookTogether = (req, res) => {
   res.render("myrecipes-cooktogether");
 };
+
+const renderCookTogetherPals = async (req, res) => {
+ const {gluten, peanut, sesame, grain, soy, sulphite, treeNut, wheat} = req.session.user.intolerances
+
+ const pals = await User.findAll({
+    attributes: ['first_name', 'last_name', 'bio'],
+    where: {
+      [Op.and]: [
+        { gluten_intolerance: gluten },
+        { grain_intolerance: grain },
+        { soy_intolerance: soy },
+        { peanut_intolerance: peanut },
+        { sesame_intolerance: sesame },
+        { sulphite_intolerance: sulphite },
+        { tree_nut_intolerance: treeNut },
+        { wheat_intolerance: wheat },
+      ],
+      id: {
+        [Op.ne] : req.session.user.id
+      }
+    },
+    raw: true,
+    nested: true
+  })
+
+console.log(pals)
+}
+
+
+
 const renderMyRecipes = async (req, res) => {
   const recipes = await Recipe.findAll({
     attributes: ['recipe_id', 'dish_name'],
@@ -24,4 +56,5 @@ module.exports = {
   renderCookTogether,
   renderMyRecipesCookTogether,
   renderMyRecipes,
+  renderCookTogetherPals,
 };
