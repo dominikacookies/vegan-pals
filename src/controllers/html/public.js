47b649174bc3aed1df5cf3bbdb359c32901a3c86
Sy C <76875297+SyreetaC@users.noreply.github.com>
2021-06-19 14:33:00 +0100

    wip

--- src/controllers/html/public.js
+++ src/controllers/html/public.js
@@ -1,6 +1,15 @@
 const axios = require("axios");
 
-const BASE_URL = `https://api.spoonacular.com/recipes/complexSearch?apiKey=a400351722ac47169e8e48e6415e0440&instructionsRequired=true&addRecipeInformation=true&fillIngredients=true&number=10&diet=vegan`;
+const COMPLEX_SEARCH_URL = `https://api.spoonacular.com/recipes/complexSearch`;
+
+const baseParams = {
+  apiKey: "a400351722ac47169e8e48e6415e0440",
+  instructionsRequired: true,
+  addRecipeInformation: true,
+  fillIngredients: true,
+  number: 10,
+  diet: "vegan",
+};
 
 const renderHomePage = (req, res) => {
   try {
@@ -43,52 +52,28 @@ const getUserIntolerances = (intolerances) =>
 const renderSearchResults = async (req, res) => {
   const { loggedIn } = req.session;
   const { query } = req.query;
-  console.log(query);
-  if (loggedIn) {
-    let intoleranceParams = "";
-    const generateIntoleranceParams = (intoleranceKeyValuePairArray) => {
-      if (!intoleranceKeyValuePairArray[1]) {
-        return;
-      }
-      if (intoleranceParams === "") {
-        intoleranceParams =
-          intoleranceKeyValuePairArray[0] == "treeNut"
-            ? "&intolerances=tree%20nut"
-            : `&intolerances=${intoleranceKeyValuePairArray[0]}`;
-      } else {
-        intoleranceKeyValuePairArray[0] == "treeNut"
-          ? (intoleranceParams += ",tree%20nut")
-          : (intoleranceParams += `,${intoleranceKeyValuePairArray[0]}`);
-      }
-    };
-    const { intolerances } = req.session.user;
-    console.log(intolerances);
 
-    Object.entries(intolerances).map(generateIntoleranceParams);
-    // console.log(intoleranceParams);
-    //
-    const response = await axios.get(
-      "https://api.spoonacular.com/recipes/complexSearch",
-      {
-        params: {
-          apiKey: "a400351722ac47169e8e48e6415e0440",
-          instructionsRequired: true,
-          addRecipeInformation: true,
-          fillIngredients: true,
-          number: 10,
-          diet: "vegan",
-          intolerances: getUserIntolerances(intolerances),
-        },
-      }
-    );
-    // console.log(response.data);
+  if (loggedIn) {
+    const { intolerances } = req.session.user;
+
+    const response = await axios.get(COMPLEX_SEARCH_URL, {
+      params: {
+        ...baseParams,
+        ...req.query,
+        intolerances: getUserIntolerances(intolerances),
+      },
+    });
+
     res.render("search-results", { data: JSON.stringify(response.data) });
   } else {
-    // const response = await axios.get(`${BASE_URL}${query}`);
-    // res.render("search-results", {
-    //   data: JSON.stringify(response.data),
-    // });
-    //map through each result
+    const response = await axios.get(COMPLEX_SEARCH_URL, {
+      params: {
+        ...baseParams,
+        ...req.query,
+      },
+    });
+
+    res.render("search-results", { data: JSON.stringify(response.data) });
   }
 };
 
