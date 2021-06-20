336c7e9375fb282559569463be5b3cd56b31441e
Misha <73886737+misha244@users.noreply.github.com>
2021-06-20 15:31:39 +0100

    get reqs, user info and recipe by id

---
+++ cook-togethers/controllers.js
@@ -0,0 +1,93 @@
+const { raw } = require("express");
+const { Op } = require("sequelize");
+
+const { CookTogether, User, Recipe } = require("../src/models");
+
+//how do I get ids from session??
+const userId = 2;
+recipeId = 798400;
+
+// Requests fns
+const getReceived = async (req, res) => {
+  try {
+    const receivedCookTogether = await CookTogether.findAll({
+      where: {
+        user_id: userId,
+        status: "received",
+      },
+      raw: true,
+      nested: true,
+    });
+    console.log(receivedCookTogether);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllUpcoming = async (req, res) => {
+  try {
+    const allUpcoming = await CookTogether.findAll({
+      where: {
+        //user_id: userId,
+        status: "accepted",
+      },
+      raw: true,
+      nested: true,
+    });
+    console.log(allUpcoming);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getNearestUpcoming = async (req, res) => {
+  try {
+    const nearestUpcoming = await CookTogether.findOne({
+      where: {
+        user_id: userId,
+        status: "accepted",
+      },
+      raw: true,
+      nested: true,
+    });
+    console.log(nearestUpcoming);
+  } catch (error) {
+    console.log(error);
+  }
+};
+// User info
+const getUserInfo = async (req, res) => {
+  try {
+    const userInfo = await User.findAll({
+      where: {
+        id: userId,
+      },
+      raw: true,
+      nested: true,
+    });
+    console.log(userInfo);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getRecipe = async (req, res) => {
+  try {
+    const recipeInfo = await Recipe.findAll({
+      where: {
+        recipe_id: recipeId,
+      },
+      raw: true,
+      nested: true,
+    });
+    console.log(recipeInfo);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//getReceived();
+//getUserInfo();
+//getRecipe();
+getAllUpcoming();
+//getNearestUpcoming();
