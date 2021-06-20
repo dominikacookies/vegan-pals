94a16948271cd45927373ea5e2fd78099a4854de
Dominika <domi_nika@hotmail.co.uk>
2021-06-20 19:07:12 +0100

    added marcus changes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,25 +1,37 @@
 console.log("hello to the client");
-
 const renderHomePageCards = () => {
     
 }
-
-
-
-
 const onSubmit = (event) => {
   event.preventDefault();
-
   const searchInput = $("#searchInput").val();
   console.log(searchInput);
+  
+    let intolerances = document.querySelectorAll(".intolerance:checked")
+    intolerances = Array.prototype.slice.call(intolerances);
+    const prepTime = $("input[type='radio']:checked").attr("id")
+    console.log(intolerances)
+     const callback = (each) => {
+        return each.name
+    }
+    const intoleranceNames = intolerances.map(callback)
+    console.log(intoleranceNames,prepTime)
+    const requestBody = {
+      searchInput, intoleranceNames,prepTime
+    }
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      body: JSON.stringify(requestBody),
+    };
+    const response = await fetch("/search-results",options)
+    
 };
-
-$(".radioButton").on("click", (event) => {
-  console.log(event.target.id);
-});
-
 $("#searchButton").on("click", onSubmit);
-
 $(document).ready(() => {
 //   renderHomePageCards();
+console.log("hi")
 });
