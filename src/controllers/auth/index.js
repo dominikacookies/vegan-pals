63f482f4d1b5143273dbf828024b4eb008f824e7
Otilia Visan <otilia.visan22@gmail.com>
2021-06-20 19:52:20 +0100

    moved signup into the main index

--- src/controllers/auth/index.js
+++ src/controllers/auth/index.js
@@ -1,43 +1,45 @@
 // TO DO: remove this import once passwordvalid method fixed
 const bcrypt = require("bcrypt");
 
-const { User } = require("../../models")
-
+const { User } = require("../../models");
 
 const login = async (req, res) => {
   try {
-    const { email , password } = req.body
+    const { email, password } = req.body;
 
     if (!email || !password) {
       return res.status(404).json({
-        error: "Required values missing."
-      })
+        error: "Required values missing.",
+      });
     }
-    
+
     const user = await User.findOne({
-      where : { email },
+      where: { email },
       raw: true,
-      nested: true
-    })
+      nested: true,
+    });
 
     if (!user) {
       return res.status(404).json({
-        error: "User does not exist"
-      })
+        error: "User does not exist",
+      });
     }
 
     // const passwordValidationResult = await user.isPasswordValid(password);
-  
+
     // TO DO: when trying to do this via model method it says that it doesn't exist
-    const passwordValidationResult = await bcrypt.compare(password, user.password); 
+    const passwordValidationResult = await bcrypt.compare(
+      password,
+      user.password
+    );
 
     if (!passwordValidationResult) {
       return res.status(404).json({
-        error: "Password invalid."
-      })
+        error: "Password invalid.",
+      });
     }
 
-    req.session.loggedIn = true
+    req.session.loggedIn = true;
 
     req.session.user = {
       id: user.id,
@@ -53,33 +55,52 @@ const login = async (req, res) => {
         sulphite: user.sulphite_intolerance,
         treeNut: user.tree_nut_intolerance,
         wheat: user.wheat_intolerance,
-      }
-    }
+      },
+    };
 
-    console.log(req.session)
-  
+    console.log(req.session);
+
     return res.status(200).json({
-      message: "Log in successful."
-    })
-  
+      message: "Log in successful.",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json({
-      error: "Unable to login."
-    })
+      error: "Unable to login.",
+    });
   }
-}
+};
 
 const logout = async (req, res) => {
-  console.log("logout")
-}
+  console.log("logout");
+};
 
 const signup = async (req, res) => {
-  console.log("signup")
-}
+  try {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const password = req.body.password;
+
+    const user = await User.create({
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      password: password,
+    });
+
+    if (!user) {
+      return res.status(500).json({ error: "Failed to signup" });
+    }
+
+    return res.status(200).json({ message: "success" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ error: "Failed to signup" });
+  }
+};
 
 module.exports = {
   login,
   logout,
-  signup
-}
+  signup,
+};
